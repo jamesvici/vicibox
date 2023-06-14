6616dc134db5c8329fed7cf4614a1a79b91caf44
James Pearson <jamesp@vicidial.com>
2023-06-14 04:28:14 -0400

    Finally fixing my mess I hope

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,17 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
 
-# Modules
+# -- Path setup --------------------------------------------------------------
 import datetime
-import sphinx_rtd_theme
 
-# -- Project information
+# -- Project information -----------------------------------------------------
 project = "ViciBox"
 copyright = "2005-%d, ViciDial Group" % datetime.datetime.now().year
 author = "James Pearson"
 release = "11.0 Beta"
 version = "11.0 Beta"
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
 extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
@@ -29,23 +28,23 @@ intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
+# -- Options for EPUB output
+epub_show_urls = "footnote"
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ["_static"]
 html_baseurl = 'https://docs.vicibox.com/'
 html_theme_options = { 'style_nav_header_background': '#00AA0D', }
 #html_logo = 'images/Vicibox_pixel_green_whitebg_20180921.png'
 html_favicon = 'images/favicon.ico'
 html_show_sourcelink = False
 htmlhelp_basename = 'viciboxdoc'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
