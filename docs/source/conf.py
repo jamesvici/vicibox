c7c453db84e15d6b663a61f149ab952e88c53185
James Pearson <jamesp@vicidial.com>
2023-06-14 05:15:12 -0400

    Remove static until needed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,14 +37,14 @@ epub_show_urls = "footnote"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ["_static"]
-html_baseurl = 'https://docs.vicibox.com/'
+html_theme = "sphinx_rtd_theme"
+#html_static_path = ["_static"]
+html_baseurl = "https://docs.vicibox.com/"
 html_theme_options = { 'style_nav_header_background': '#00AA0D', }
 #html_logo = 'images/Vicibox_pixel_green_whitebg_20180921.png'
-html_favicon = 'images/favicon.ico'
+html_favicon = "images/favicon.ico"
 html_show_sourcelink = False
-htmlhelp_basename = 'viciboxdoc'
+htmlhelp_basename = "viciboxdoc"
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
